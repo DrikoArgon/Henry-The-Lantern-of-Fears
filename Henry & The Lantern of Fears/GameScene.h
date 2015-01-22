b5909c3006ae98d8097b438de944bd8f5f321829
Adriano Alves Ribeiro Gonçalves <adrianogoncalves@Adrianos-MacBook-Pro.local>
2015-01-22 12:53:47 -0200

    Initial Commit

---
+++ Henry & The Lantern of Fears/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Henry & The Lantern of Fears
+//
+
+//  Copyright (c) 2015 ABHI. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
