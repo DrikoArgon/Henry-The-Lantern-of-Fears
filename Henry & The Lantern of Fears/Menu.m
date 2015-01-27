eacbed2bf3900b35a82da03e86b344b71a6ec07b
Drikoso <driko.argon@gmail.com>
2015-01-27 14:38:14 -0200

    Revert 398d512..80dc97f
    This rolls back to commit 398d512d35f8b36fabb65721a72806b071fb67f0.

--- Henry & The Lantern of Fears/Menu.m
+++ Henry & The Lantern of Fears/Menu.m
@@ -25,7 +25,7 @@
         label.name = @"start";
         
         [self addChild:label];
-        [self animateWithPulse: label];
+        [self animateWithPulse:label];
         
     }
     return self;
@@ -33,16 +33,12 @@
 
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 398d512... Tela 1 e Tela 2 modificadas
+<<<<<<< Updated upstream
     for (UITouch *touch in touches) {
         SKNode *n = [self nodeAtPoint:[touch locationInNode:self]];
         if([n.name isEqualToString:@"start"]){
             NSLog(@"mundou pro mundo");
-<<<<<<< HEAD
-
+=======
     NSLog(@"entrou1");
     for(UITouch *touch in touches)
       {
@@ -53,9 +49,7 @@
             
             NSLog(@"entrou3");
             
-
-=======
->>>>>>> parent of 398d512... Tela 1 e Tela 2 modificadas
+>>>>>>> Stashed changes
             
             Mundos *scene = [[Mundos alloc] initWithSize:self.view.bounds.size];
 //            scene.size = skView.frame.size;
@@ -69,6 +63,7 @@
         }
     }
 }
+
 -(void)animateWithPulse:(SKNode *)node
 {
     SKAction *disappear = [SKAction fadeAlphaTo:0.0 duration:0.3];
@@ -76,9 +71,13 @@
     SKAction *pulse = [SKAction sequence:@[disappear,appear]];
     [node runAction:[SKAction repeatActionForever:pulse]];
 }
-<<<<<<< HEAD
-    
+
+<<<<<<< Updated upstream
 =======
 
->>>>>>> parent of 398d512... Tela 1 e Tela 2 modificadas
+
+>>>>>>> Stashed changes
 @end
+
+
+
